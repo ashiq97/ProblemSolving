857525292be18274933d9c33d5b3339e6f63d4de
ashiq <ashiqur.khan@technonext.com>
2024-04-16 18:01:45 +0600

    leet code 242 valid anagram

--- ProblemSolving_sln/Program.cs
+++ ProblemSolving_sln/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Leetcode_217_Contains_Duplicate;
+using Leetcode_242_Valid_Anagram;
 
-
+//1. Leetcode 217
 var ContainsDuplicate_result = ContainsDuplicate.GetResult();
 Console.WriteLine(ContainsDuplicate_result);
+
+//2. Leetcode 242
+var IsAngram_result = ValidAnagram.GetResult();
+Console.WriteLine(IsAngram_result);
