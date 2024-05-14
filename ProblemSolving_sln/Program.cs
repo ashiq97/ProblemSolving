5098f5975d7c8858e6af7dd635e333a1ed1c9dc2
ashiq <ashiqur.khan@technonext.com>
2024-05-14 23:26:13 +0600

    Valid Palindrome using Stack

--- ProblemSolving_sln/Program.cs
+++ ProblemSolving_sln/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Leetcode_125_Valid_Palindrome;
 using Leetcode_128_longest_consecutive_sequence;
 using Leetcode_217_Contains_Duplicate;
 using Leetcode_242_Valid_Anagram;
@@ -14,3 +15,7 @@ Console.WriteLine(IsAngram_result);
 //3. Leetcode 128
 var LongestConsecutive_result = LongestConsecutiveSequence.GetResult();
 Console.WriteLine(LongestConsecutive_result);
+
+//4.Leetcode 125
+var IsPalindrome_result = ValidPalindrome.IsPalindrome();
+Console.WriteLine(IsPalindrome_result);
